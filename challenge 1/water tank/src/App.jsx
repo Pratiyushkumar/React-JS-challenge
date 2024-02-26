5cd7fe3365fcbac182861fe69db71342404a8ac8
Pratiyush Kumar <82165483+Pratiyushkumar@users.noreply.github.com>
2024-02-26 04:37:33 +0000

    water balancer ui creation is done

--- challenge 1/water tank/src/App.jsx
+++ challenge 1/water tank/src/App.jsx
@@ -4,8 +4,10 @@ import WaterTank from "./WaterTank";
 function App() {
   return (
     <>
-      <h1>Water Balancer</h1>
-      <div>
+      <h1 style={{fontFamily:"monospace"}}>Water Balancer</h1>
+      <div className="water__tank__container">
+        <WaterTank />
+        <WaterTank />
         <WaterTank />
       </div>
     </>
--- challenge 1/water tank/src/WaterTank.jsx
+++ challenge 1/water tank/src/WaterTank.jsx
@@ -3,8 +3,8 @@ import "./watertank.css";
 const WaterTank = () => {
   return (
     <div className="container">
-      <button>Add Water</button>
-      <button>Remove Water</button>
+      <button className="add__button">Add Water</button>
+      <button className="remove__button">Remove Water</button>
       <div className="water__tank"></div>
     </div>
   );
