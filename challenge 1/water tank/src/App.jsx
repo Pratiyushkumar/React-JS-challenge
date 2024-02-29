f416da0a934b959bc59def1f2c10ae3ffea5e613
Pratiyush Kumar <82165483+Pratiyushkumar@users.noreply.github.com>
2024-02-29 03:11:42 +0000

    added useref, usestate hook

--- challenge 1/water tank/src/App.jsx
+++ challenge 1/water tank/src/App.jsx
@@ -1,15 +1,46 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import WaterTank from "./WaterTank";
 
 function App() {
+  const [waterLevel, setWaterLevel] = useState(0);
+  const waterTank1Ref = useRef(null);
+  const waterTank2Ref = useRef(null);
+  const waterTank3Ref = useRef(null);
+
+  const handleAddWaterToTank = (e) => {
+    console.log("adding water to tank");
+    console.log(waterTankRef);
+    console.log(waterTankRef.current.dataset.tank);
+  }
+
+  const handleEmptyWaterFromTank = (e) => {
+    console.log("emptying the tank");
+  }
+
   return (
     <>
-      <h1 style={{fontFamily:"monospace"}}>Water Balancer</h1>
+      <h1 style={{ fontFamily: "monospace" }}>Water Balancer</h1>
       <div className="water__tank__container">
-        <WaterTank />
-        <WaterTank />
-        <WaterTank />
+        <WaterTank tank={1}
+          addWater={handleAddWaterToTank}
+          emptyTank={handleEmptyWaterFromTank}
+          level={waterLevel}
+          waterTankRef={waterTank1Ref} />
+
+        <WaterTank tank={2}
+          addWater={handleAddWaterToTank}
+          emptyTank={handleEmptyWaterFromTank}
+          level={waterLevel}
+          waterTankRef={waterTank2Ref} />
+
+        <WaterTank tank={3}
+          addWater={handleAddWaterToTank}
+          emptyTank={handleEmptyWaterFromTank}
+          level={waterLevel}
+          waterTankRef={waterTank3Ref} />
       </div>
+
     </>
   );
 }
--- challenge 1/water tank/src/WaterTank.jsx
+++ challenge 1/water tank/src/WaterTank.jsx
@@ -1,12 +1,15 @@
 import "./watertank.css";
 
-const WaterTank = () => {
+const WaterTank = ({addWater, emptyTank, level, tank, waterTankRef}) => {
   return (
-    <div className="container">
-      <button className="add__button">Add Water</button>
-      <button className="remove__button">Remove Water</button>
-      <div className="water__tank"></div>
-    </div>
+    <>
+      <div className="container">
+        <button className="add__button" onClick={()=>addWater()}>Add Water</button>
+        <button className="remove__button"  onClick={()=>emptyTank()}>Remove Water</button>
+        <div className="water__tank" data-tank={tank} ref={waterTankRef}></div>
+        <p className="percentage">{level}%</p>
+      </div>
+    </>
   );
 };
 
